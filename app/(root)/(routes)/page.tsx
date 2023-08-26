da331e9218fcebdc2a2da3b1cb8c29c9d1298378
ahoang2502 <athoang2502@gmail.com>
2023-08-26 17:14:10 +1000

    ...

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,14 @@
+import Categories from "@/components/Categories";
 import SearchInput from "@/components/SearchInput";
+import prismadb from "@/lib/prismadb";
 
+export default async function Home() {
+  const categories = await prismadb.category.findMany();
 
-export default function Home() {
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 }
